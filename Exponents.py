25dcf3bd988c7bb61342b9de5e57f8904447cc2e
Akshay Sanjay Mulik <akshaymulik399@gmail.com>
2018-08-06 23:40:48 +0530

    Create Exponents.py

---
+++ Exponents.py
@@ -0,0 +1,12 @@
+# Accept the value of n
+n = input(" Enter the Value of n: ")
+
+# n^2,n^3 and n^4
+n2 = pow(int(n),2)
+n3 = pow(int(n),3)
+n4 = pow(int(n),4)
+
+#Print the Answer
+print(' n^2 is {0}'.format(n2))
+print(' n^3 is {0}'.format(n3))
+print(' n^4 is {0}'.format(n4))
